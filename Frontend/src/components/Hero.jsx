667f6aeed8a38fefdc65a52d072481096f0892ee
mo7amedgisimelsied <mohamedgisimelsied@gmail.com>
2025-08-17 00:44:09 +0400

    React.js frontend added

---
+++ Frontend/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero(){
+    return (
+        <header>
+            <h1>Creativity shouldn’t come with a price tag</h1>
+            <h2>No paywalls. No tricks. Just free assets from designers who believe creativity should be open to everyone.</h2>
+            
+        </header>
+    );
+}
+
+export default Hero;
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar(){
+
+    return (<nav>
+        <p>OpenAssets</p>
+        <ul>
+            <li>Home</li>
+            <li>Icons</li>
+            <li>3D</li>
+            <li>Vector</li>
+        </ul>
+    </nav>);
+
+}
+export default Navbar;
