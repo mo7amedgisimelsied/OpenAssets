550375553b53f3d1b6bed2986b6c705cbdf14019
mo7amedgisimelsied <mohamedgisimelsied@gmail.com>
2025-08-17 07:30:07 +0400

    added contribution + donation sections on homepage

--- Frontend/src/components/Contibution.jsx
+++ Frontend/src/components/Contibution.jsx
@@ -1,28 +1,29 @@
 import discordIcon from '../../public/discord-icon.svg';
+import '../styles/contribution-styles.css';
 function Contribution() {
     return (
-        <section>
+        <section className="contribution-section">
             <h2>WANT TO GIVE BACK?</h2>
-            <p>Whether you’re a 3D artist, icon designer, or illustrator, you can help creators everywhere by sharing your work for free. Join our global team of volunteers and make the creative world a little fairer.</p>
-            <button>
+            <p className='contribution-subtext'>Whether you’re a 3D artist, icon designer, or illustrator, you can help creators everywhere by sharing your work for free. Join our global team of volunteers and make the creative world a little fairer.</p>
+            <button className='join-discord'>
                 <img src={discordIcon} alt="Discord Icon" />
-                Join the Discord
+                Join the Discord →
             </button>
 
-            <div>
-                <div>
-                <p>128+</p>
-                <p>Designers</p>
+            <div className='site-statistics'>
+                <div className='statistics'>
+                <p className='number'>128+</p>
+                <p className='label'>Designers</p>
                 </div>
 
-                <div>
-                <p>4,672+</p>
-                <p>Free Assets</p>
+                <div className='statistics'>
+                <p className='number'>4,672+</p>
+                <p className='label'>Free Assets</p>
                 </div>
 
-                <div>
-                <p>1.2M+</p>
-                <p>Downloads</p>
+                <div className='statistics'>
+                <p className='number'>1.2M+</p>
+                <p className='label'>Downloads</p>
                 </div>
 
             </div>
--- Frontend/src/components/Donation.jsx
+++ Frontend/src/components/Donation.jsx
@@ -1,16 +1,18 @@
+import '../styles/donation-styles.css';
 function Donation() {
     return (
-        <section>
+        <section className="donation-section">
             <h2>Keep creativity free for everyone</h2>
-            <p>We run on love, not ads, but servers and storage still cost money. Your donation helps us keep providing free, high-quality assets to anyone who needs them.</p>
+            <p className='donation-subtext'>We run on love, not ads, but servers and storage still cost money. Your donation helps us keep providing free, high-quality assets to anyone who needs them.</p>
 
-            <p>Top Supporters</p>
-            <div>
+            <p className='top-supporters'>Top Supporters</p>
+            <div className='supporters-pics'>
                 <img src="https://placehold.co/100x100" alt="supporter" />
-                <img src="https://placehold.co/100x100" alt="supporter" />
-                <img src="https://placehold.co/100x100" alt="supporter" />
-                <img src="https://placehold.co/100x100" alt="supporter" />
+                <img src="https://placehold.co/200x200" alt="supporter" />
+                <img src="https://placehold.co/200x200" alt="supporter" />
+                <img src="https://placehold.co/200x200" alt="supporter" />
             </div>
+            <button className='donate-button'>Donate Now</button>
         </section>
     );
 }
