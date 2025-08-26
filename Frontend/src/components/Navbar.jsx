eda2e6b3fc9e38c98d9bcfc670fc7dd425394118
mo7amedgisimelsied <mohamedgisimelsied@gmail.com>
2025-08-26 09:49:24 +0400

    fixed navbar responsiveness

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 function Navbar(props){
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
     let history = useNavigate();
     function navigateTo(path) {
         history(`/${path}`);
+        setIsMenuOpen(false);
     }
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (<nav>
         <p>OpenAssets</p>
-        <ul>
+        <button className="hamburger" onClick={toggleMenu}>
+                ☰
+            </button>
+        <ul className={isMenuOpen ? "nav-links-open" : "nav-links-closed"}>
             <li 
             style={
                 props.current === 'Home'? {borderBottom: "solid 1px"}:{border: "none"}
